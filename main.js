e1715968d995b9d5bb786296017775b87dc540bd
Dhvani1003 <76445736+Dhvani1003@users.noreply.github.com>
2021-06-07 16:41:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -15,33 +15,10 @@ leftWristX = 0;
   poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded() {
-  console.log('PoseNet Is Initialized!');
-}
-
-
 function gotPoses(results)
 {
   if(results.length > 0)
   {
     console.log(results);
-    noseX = results[0].pose.nose.x;
-    noseY = results[0].pose.nose.y;
-    console.log("noseX = " + noseX +" noseY = " + noseY);
-
-    leftWristX = results[0].pose.leftWrist.x;
-    rightWristX = results[0].pose.rightWrist.x;
-    difference = floor(leftWristX - rightWristX);
-
-    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
   }
-}
-
-function draw() {
-background('#969A97');
-
-  document.getElementById("font_size").innerHTML = "Size of the font will be = " + difference +"px";
-  fill('#F90093');
-  stroke('#F90093');
-  square(noseX, noseY, difference);
 }
